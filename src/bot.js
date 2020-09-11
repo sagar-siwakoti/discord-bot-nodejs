409ae1ed959ac535034450a1a671cba516c9decb
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-11 19:20:35 +0545

    -added kick command

--- src/bot.js
+++ src/bot.js
@@ -12,7 +12,17 @@ if (message.content.startsWith(PREFIX)){
     const [CMD_NAME,...args]=message.content
         .trim()
         .substring(PREFIX.length)
-        .split(/\s+/)
+        .split(/\s+/);
+    if (CMD_NAME=='kick'){
+        if (args.length===0)
+            return  message.reply('Please provide an id');
+        const member=message.guild.members.cache.get(args[0]);
+        if (member){
+            member.kick();
+        }else {
+            message.channel.send('that member was not found');
+        }
+    }
 }
 
 });
