c823c9851a990d0a645cee5836c8d34335df30ae
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-11 19:39:20 +0545

    -added and completed the role feature

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,106 @@
 require("dotenv").config();
 
-const { Client } = require("discord.js");
-const client = new Client();
-const PREFIX="$";
-client.on("ready", () => {
-  console.log(`${client.user.tag} has logged in`);
-});
-client.on('message',(message)=>{
-    if (message.author.bot) return;
-if (message.content.startsWith(PREFIX)){
-    const [CMD_NAME,...args]=message.content
-        .trim()
-        .substring(PREFIX.length)
-        .split(/\s+/);
-    if (CMD_NAME=='kick'){
-        if (args.length===0)
-            return  message.reply('Please provide an id');
-        const member=message.guild.members.cache.get(args[0]);
-        if (member){
-            member.kick();
-        }else {
-            message.channel.send('that member was not found');
-        }
-    }
-}
+const { Client, WebhookClient } = require("discord.js");
 
+const client = new Client({
+  partials: ["MESSAGE", "REACTION"],
 });
-client.login(process.env["DISCORDJS-BOT-TOKEN"]);
+
+const webhookClient = new WebhookClient(
+  process.env.WEBHOOK_ID,
+  process.env.WEBHOOK_TOKEN
+);
+
+const PREFIX = "$";
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    if (CMD_NAME === "kick") {
+      if (!message.member.hasPermission("KICK_MEMBERS"))
+        return message.reply("You do not have permissions to use that command");
+      if (args.length === 0) return message.reply("Please provide an ID");
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member
+          .kick()
+          .then((member) => message.channel.send(`${member} was kicked.`))
+          .catch((err) => message.channel.send("I cannot kick that user :("));
+      } else {
+        message.channel.send("That member was not found");
+      }
+    } else if (CMD_NAME === "ban") {
+      if (!message.member.hasPermission("BAN_MEMBERS"))
+        return message.reply(
+          "You do not have permissions to use that command on this channel"
+        );
+      if (args.length === 0) return message.reply("Please provide an ID");
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send("User was banned successfully.");
+      } catch (err) {
+        console.log(err);
+        message.channel.send(
+          "An error occured . Either you do not have permissions or the user was not found"
+        );
+      }
+    } else if (CMD_NAME === "announce") {
+      console.log(args);
+      const msg = args.join(" ");
+      console.log(msg);
+      webhookClient.send(msg);
+    }
+  }
+});
+
+client.on("messageReactionAdd", (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === "738666523408990258") {
+    switch (name) {
+      case "🍎":
+        member.roles.add("738664659103776818");
+        break;
+      case "🍌":
+        member.roles.add("738664632838782998");
+        break;
+      case "🍇":
+        member.roles.add("738664618511171634");
+        break;
+      case "🍑":
+        member.roles.add("738664590178779167");
+        break;
+    }
+  }
+});
+
+client.on("messageReactionRemove", (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === "738666523408990258") {
+    switch (name) {
+      case "🍎":
+        member.roles.remove("738664659103776818"); //message id
+        break;
+      case "🍌":
+        member.roles.remove("738664632838782998");
+        break;
+      case "🍇":
+        member.roles.remove("738664618511171634");
+        break;
+      case "🍑":
+        member.roles.remove("738664590178779167");
+        break;
+    }
+  }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
