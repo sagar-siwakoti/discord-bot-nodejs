57572d45b5cad36a02a6da584e238ac0b60a6ebe
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-11 18:59:21 +0545

    -added bot response

--- src/bot.js
+++ src/bot.js
@@ -1,2 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
+const { Client } = require("discord.js");
+const client = new Client();
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in`);
+});
+client.on('message',(message)=>{
+    if (message.author.bot) return;
+
+
+});
+client.login(process.env["DISCORDJS-BOT-TOKEN"]);
