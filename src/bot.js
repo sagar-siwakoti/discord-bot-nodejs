eeb27059e7e41b9325c3532ab7098d1ac11e5e8d
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-11 19:08:07 +0545

    -added command prefix

--- src/bot.js
+++ src/bot.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 
 const { Client } = require("discord.js");
 const client = new Client();
-
+const PREFIX="$";
 client.on("ready", () => {
   console.log(`${client.user.tag} has logged in`);
 });
 client.on('message',(message)=>{
     if (message.author.bot) return;
-
+if (message.content.startsWith(PREFIX)){
+    const [CMD_NAME,...args]=message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/)
+}
 
 });
 client.login(process.env["DISCORDJS-BOT-TOKEN"]);
